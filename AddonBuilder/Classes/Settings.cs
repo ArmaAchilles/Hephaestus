ed94709cd7cc73e63cf573800d6790b2e8ecfd35
CreepPork <creepporkstudios@gmail.com>
2018-09-28 18:21:31 +0300

    Development I can't be bothered to split the commits into proper ones

---
+++ AddonBuilder/Classes/Settings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hephaestus.Classes
+{
+    public class Settings
+    {
+        public class Directories
+        {
+            public string SourceDirectory { get; set; }
+            public string TargetDirectory { get; set; }
+
+            public string AddonBuilderDirectory { get; set; }
+        }
+
+        public string ProjectPrefix { get; set; }
+
+        public class Signing
+        {
+            public string PrivateKeyDirectory { get; set; }
+            public string PrivateKeyPrefix { get; set; }
+            public string PrivateKeyDefaultVersion { get; set; }
+        }
+
+        public class GameInformation
+        {
+            public string GameFolder { get; set; }
+            public string GameExecutable { get; set; }
+            public string GameExecutableArguments { get; set; }
+
+            public bool ShutdownGameBeforeBuilding { get; set; }
+            public bool StartGameAfterBuilding { get; set; }
+        }
+
+        public class Checksums
+        {
+            //
+        }
+    }
+}
--- AddonBuilder/Program.cs
+++ AddonBuilder/Program.cs
@@ -1,9 +1,44 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace Hephaestus
 {
     public class Program
     {
-        
+        public static void Main()
+        {
+            HandleJson();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        public static void HandleJson()
+        {
+            ReadJson();
+        }
+
+        public static void ReadJson()
+        {
+
+        }
+
+        // Read the JSON
+
+        // If default then open Hephaestus Configurator
+
+        // Check if all entries in JSON are true
+
+        // Get all the PBOs
+
+        // Check if they were previously built
+
+        // Close Arma 3 (if set so)
+
+        // Build changed PBOs
+
+        // Launch Arma 3
+
+        // Exit
     }
 }
