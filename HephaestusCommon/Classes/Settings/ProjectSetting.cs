dd6c11bdc5162b618e2ae6a770f4d6a942dff54a
Ralfs <creepporkstudios@gmail.com>
2018-10-05 23:50:36 +0300

    Added JSON setting writing and reading

---
+++ HephaestusCommon/Classes/Settings/ProjectSetting.cs
@@ -0,0 +1,62 @@
+﻿using HephaestusCommon.Utilities;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace HephaestusCommon.Classes.Settings
+{
+    public class ProjectSetting
+    {
+        public string ProjectDirectory { get; set; }
+
+        public string SourceDirectory { get; set; }
+        public string TargetDirectory { get; set; }
+
+        public string AddonBuilderFile { get; set; }
+
+        public string ProjectPrefix { get; set; }
+
+        public string PrivateKeyFile { get; set; }
+
+        public string GameExecutable { get; set; }
+        public string GameExecutableArguments { get; set; }
+
+        public bool ShutdownGameBeforeBuilding { get; set; }
+        public bool StartGameAfterBuilding { get; set; }
+
+        // Folder name, checksum
+        Dictionary<string, string> Checksums { get; set; }
+
+        public ProjectSetting GetSetting()
+        {
+            ProjectSetting projectSetting = null;
+
+            MasterSetting masterSetting = MasterSetting.GetSetting();
+
+            string projectDirectory = "";
+            foreach (string directory in masterSetting.ProjectDirectories)
+            {
+                if (directory == ProjectDirectory)
+                {
+                    projectDirectory = directory;
+                }
+            }
+
+            string jsonPath = JsonUtility.GetJsonPath(JsonUtility.EJsonType.Project, projectDirectory);
+
+            if (jsonPath != null)
+            {
+                projectSetting = JsonConvert.DeserializeObject<ProjectSetting>(File.ReadAllText(jsonPath));
+            }
+
+            return projectSetting;
+        }
+
+        public void SetSetting(ProjectSetting projectSetting)
+        {
+            string json = JsonConvert.SerializeObject(projectSetting, Formatting.Indented);
+
+            JsonUtility.WriteJsonFile(JsonUtility.EJsonType.Project, json, projectSetting.ProjectDirectory);
+        }
+    }
+}
---
+++ HephaestusCommon/Utilities/JsonUtility.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HephaestusCommon.Utilities
+{
+    public static class JsonUtility
+    {
+        public static string GetJsonPath(EJsonType jsonType = EJsonType.Master, string path = "")
+        {
+            string appPath = AppDomain.CurrentDomain.BaseDirectory;
+            string jsonPath = "";
+
+            switch (jsonType)
+            {
+                case EJsonType.Master:
+                    jsonPath = String.Format("{0}\\.hephaestus.master.json", appPath);
+
+                    break;
+
+                case EJsonType.Project:
+                    jsonPath = String.Format("{0}\\.hephaestus.json", path);
+
+                    break;
+            }
+
+            if (! File.Exists(jsonPath))
+            {
+                jsonPath = null;
+            }
+
+            return jsonPath;
+        }
+
+        public static void WriteJsonFile(EJsonType jsonType, string json, string path = "")
+        {
+            string jsonPath = GetJsonPath(jsonType, path);
+
+            File.WriteAllText(jsonPath, json);
+        }
+
+        public enum EJsonType
+        {
+            Master = 0,
+            Project = 1
+        }
+    }
+}
