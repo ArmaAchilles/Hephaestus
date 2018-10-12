baaeb3a0c936f3fc61548705412a87908f2a5fe2
Ralfs <creepporkstudios@gmail.com>
2018-10-12 15:03:09 +0300

    Reworked settings

---
+++ HephaestusCommon/Classes/Project.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace HephaestusCommon.Classes
+{
+    public class Project
+    {
+        public string ProjectDirectory { get; set; }
+
+        public string SourceDirectory { get; set; }
+        public string TargetDirectory { get; set; }
+
+        public string AddonBuilderFile { get; set; }
+
+        public string ProjectPrefix { get; set; }
+
+        public string PrivateKeyFile { get; set; }
+
+        public string GameExecutable { get; set; }
+        public string GameExecutableArguments { get; set; }
+
+        public bool ShutdownGameBeforeBuilding { get; set; }
+        public bool StartGameAfterBuilding { get; set; }
+
+        // Folder name, checksum
+        Dictionary<string, string> Checksums { get; set; }
+    }
+}
---
+++ HephaestusCommon/Utilities/ProjectUtility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using HephaestusCommon.Classes;
+using Newtonsoft.Json;
+
+namespace HephaestusCommon.Utilities
+{
+    public class ProjectUtility
+    {
+        private string GetJsonPath(string path)
+        {
+            return String.Format("{0}\\.hephaestus.json", path);
+        }
+
+        public Project GetProject(string path)
+        {
+            Project project;
+
+            string jsonPath = GetJsonPath(path);
+
+            if (File.Exists(jsonPath))
+            {
+                project = JsonConvert.DeserializeObject<Project>(File.ReadAllText(jsonPath));
+            }
+            else
+            {
+                project = null;
+            }
+
+            return project;
+        }
+
+        public void SetProject(string path, Project project)
+        {
+            string jsonPath = GetJsonPath(path);
+
+            string json = JsonConvert.SerializeObject(project, Formatting.Indented);
+
+            File.WriteAllText(jsonPath, json);
+        }
+    }
+}
